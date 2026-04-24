2233bf791ba877cfd3f73b20533e2914fe69fcb2
edwin-hidalgo <hidalgodedwin@gmail.com>
2026-04-24 14:13:14 -0400

    chore: add Vercel domain to allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['127.0.0.1', 'spongy-evil-wrinkly.ngrok-free.dev'],
+  allowedDevOrigins: ['127.0.0.1', 'spongy-evil-wrinkly.ngrok-free.dev', 'verified-fan-app.vercel.app'],
 };
 
 export default nextConfig;
